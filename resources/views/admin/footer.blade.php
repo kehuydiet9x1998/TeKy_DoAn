780c33df00b3508b296f4195af9cacf2105a89f6
unknown <nguyenlinh1998adhp@gmail.com>
2020-09-26 06:25:50 +0700

    Template_TeKy_DoAn

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2020-2021 <a href="http://teky.edu.vn">TeKy_VN</a>.</strong>
+    Develop by KimAnh Team.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0.0
+    </div>
+</footer>
